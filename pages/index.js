34399de863dc66ff238efcea09115839d39f20fd
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-04-01 17:32:16 -0300

    Onde colocar meu CSS?

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import Link from '../src/components/Link';
+
+function Title({ children, as = 'h1' }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: red;
+          font-family: sans-serif;
+        }
+      `}</style>
+    </React.Fragment>
+  );
+}
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Alura Case - Divulgação</h1>
+      <Title as="h2">Alura Case - Divulgação</Title>
       <Link href="/faq" passHref>Faq</Link>
     </div>
   );
