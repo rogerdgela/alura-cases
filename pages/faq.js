7aa9cb3e1ad2a6df15403b526369419f0eff9190
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-04-04 17:51:24 -0300

    getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export default function FAQPage() {
+export async function getStaticProps() {
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faqResponse = await fetch(FAQ_API_URL);
+    const faq = await faqResponse.json();
+    return {
+        props: { faqs: faq }
+    }
+}
+export default function FAQPage({ faqs }) {
+
     return (
         <div>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
             <Link href="/">
                 Ir para a home
             </Link>
+            <ul>
+                {faqs.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
