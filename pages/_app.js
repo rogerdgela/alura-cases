745b5968440130d2366524da28928195d4612118
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-04-09 16:21:30 -0300

    Next e Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,19 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-2QZDPX3BLK');
+                        `
+                    }}
+                />
+
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet" />
