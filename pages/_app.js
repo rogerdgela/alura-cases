fe2ada36bdb2d51797aeddcb489413ab8871a997
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-04-09 15:55:42 -0300

    Como manipular o Head das páginas

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import GlobalStyle from '../src/theme/GlobalStyle';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet" />
+            </Head>
             <GlobalStyle />
             <Component {...pageProps} />
         </>
